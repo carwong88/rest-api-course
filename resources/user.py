1bd7bace79b74eb60dad8fcdb652427ed58e82b0
carwong88 <carwong88@gmail.com>
2022-03-09 10:39:17 -0800

    Added root path /.

--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask import request
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import create_access_token, create_refresh_token
--- rest_app.py
+++ rest_app.py
@@ -32,6 +32,13 @@ api.add_resource(User, '/<string:username>/remove')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Stores, '/stores')
 
+text = ('<p>Hello World! This is my Udemy REST API course project.</p>'
+'/stores -- to list all the stores')
+
+@app.route('/')
+def index():
+    return text
+
 if __name__ == '__main__':
     db.init_app(app)
     app.run(port=8888, debug=True)
